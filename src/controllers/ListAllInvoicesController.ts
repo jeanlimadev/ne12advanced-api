f85fbf966af0c7c4ddf18ec70cbc66bd903c0b9c
Jean Lima <jeanlima.dev@gmail.com>
2022-09-27 09:00:42 -0300

    feature: route and controller to list all invoices

---
+++ src/controllers/ListAllInvoicesController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import { prisma } from "../database";
+
+
+class ListAllInvoicesController {
+  async handle(request:Request, response:Response): Promise<Response> {
+    const allInvoices = await prisma.invoice.findMany();
+
+    return response.json(allInvoices);
+  }
+}
+
+export { ListAllInvoicesController };
--- src/routes/invoices.routes.ts
+++ src/routes/invoices.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 
 import { CreateInvoiceController } from "../controllers/CreateInvoiceController";
+import { ListAllInvoicesController } from "../controllers/ListAllInvoicesController";
 
 const invoicesRoutes = Router();
 
 const createInvoiceController = new CreateInvoiceController();
+const listAllInvoicescontroller = new ListAllInvoicesController();
 
 invoicesRoutes.post("/", createInvoiceController.handle);
+
+invoicesRoutes.get("/", listAllInvoicescontroller.handle);
 
 export { invoicesRoutes };
