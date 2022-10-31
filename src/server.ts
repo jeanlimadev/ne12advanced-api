c57bb4a6a19bf14a62c8287ce6df511cd10f3531
Jean Lima <jeanlima.dev@gmail.com>
2022-10-31 16:11:51 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -5,26 +5,7 @@ import { router } from "./routes";
 
 const app = express();
 
-const allowedOrigins = [
-  'http://localhost',
-  'http://127.0.0.1:8080',
-  'http://127.0.0.1:5500',
-  'https://www.ne12.bradesconetempresa.b.br'
-];
-
-app.use(cors({
-  origin: function(origin, callback) {
-    let allowed = true
-
-    // permite conexões sem origin. Exemplo: Mobile Apps
-    if (!origin) allowed = true
-
-    // verifica se o origin da request consta na lista de autorizados no array allowedOrigins
-    if (!allowedOrigins.includes(origin)) allowed = false
-
-    callback(null, allowed)
-  }
-}));
+app.use(cors());
 
 app.use(express.json());
 
